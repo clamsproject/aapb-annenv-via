710e01821fc78211828d5f292a3ca002b6b3ce51
Kelley Lynch <kelleylynch@gmail.com>
2022-07-18 13:13:11 -0400

    modifying original upload script for via annotations

--- upload.sh
+++ upload.sh
@@ -1,18 +1,18 @@
 #! /bin/bash 
 
 if [ $# -ne 2 ]; then 
-    echo "Specify a project name and a directory with data files" 
+    echo "Specify a project name and a directory with annotation files saved from via." 
     exit 1
 fi
 proj_name="$(basename $1)"
 data_dir="$2"
 
-git clone git@github.com:clamsproject/clams-aapb-annotations.git r∏∏epo
+git clone git@github.com:clamsproject/clams-aapb-annotations.git repo
 
 cd repo
 dest_dir=uploads/$proj_name/annotations
 mkdir -p $dest_dir
-for ann in $data_dir/*.ann; do 
+for ann in $data_dir/*.json; do 
     if [ -s $ann ]; then 
         cp $ann $dest_dir/
     fi
